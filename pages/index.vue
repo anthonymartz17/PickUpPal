28ba70c530b51fc16ffa7ac11f2205fe15ef1317
Antonio <anthonymartz17@hotmail.com>
2023-09-09 19:17:43 -0400

    added google maps api key to axios config

---
+++ pages/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <!-- <Tutorial /> -->
+    <div>
+      asdfads
+      {{ maps }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  data() {
+    return {
+      maps:null,
+    }
+  },
+  async asyncData(context) {
+    try {
+      const response = await context.$axios.$get({
+        // headers: {
+          //   Authorization: 'Bearer YOUR_API_KEY',
+          // },
+        })
+        console.log(response)
+
+      // Handle the response data
+      return { maps: response }
+
+      // ...
+    } catch (error) {
+      // Handle errors
+      console.error('Error:', error)
+    }
+  },
+}
+</script>
